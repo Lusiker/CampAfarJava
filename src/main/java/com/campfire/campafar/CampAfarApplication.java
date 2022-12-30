2fc675cb411736999dd24a16a08db5aa4517bf8e
Lusiker <1044114668@qq.com>
2022-12-30 11:37:02 +0800

    initial files

---
+++ src/main/java/com/campfire/campafar/CampAfarApplication.java
@@ -0,0 +1,13 @@
+package com.campfire.campafar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampAfarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampAfarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/campfire/campafar/CampAfarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campfire.campafar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampAfarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
