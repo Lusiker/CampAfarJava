f31cc1e134197a230357832352034d354520915e
Lusiker <1044114668@qq.com>
2023-01-02 16:22:47 +0800

    Initial Entities & Relations

---
+++ src/main/java/com/campfire/campafar/Entity/Question.java
@@ -0,0 +1,13 @@
+package com.campfire.campafar.Entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class Question {
+}
---
+++ src/main/java/com/campfire/campafar/Entity/Relation/UserLikeArticle.java
@@ -0,0 +1,13 @@
+package com.campfire.campafar.Entity.Relation;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class UserLikeArticle {
+}
