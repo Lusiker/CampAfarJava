5691910489d1fc058aa081b268e2349673cc0158
Lusiker <1044114668@qq.com>
2023-01-03 19:24:22 +0800

    Update create_table_question.sql

--- src/main/resources/sql/create_table_question.sql
+++ src/main/resources/sql/create_table_question.sql
@@ -26,6 +26,7 @@ CREATE TABLE `question` (
   `user_id_to` int unsigned NOT NULL, #回答用户uid
   `question_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, #提问id
   `question_created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP, #提问创建于
+  `question_last_modified` timestamp NULL DEFAULT NULL, #提问上次编辑时间
   `question_title` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL, #提问标题
   `question_detail` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '', #提问详情
   `question_tag` int(32) unsigned not null default '0', #问题标签，标签最多4个（0~63），通过数字形式存放00000000~63636363，每次取模100得到一个标签
@@ -36,6 +37,7 @@ CREATE TABLE `question` (
   `question_state` tinyint(2) DEFAULT '0', #问题状态，正常，已删除，审核中
   `question_visible` tinyint(1) DEFAULT '1', #问题可见性
   `answer_detail` varchar(1000) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '尚未回答', # 富文本形式的回答
+  `answer_last_modifier` timestamp NULL DEFAULT NULL, #回答上次编辑时间
   PRIMARY KEY (`question_id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
