62e5f674d5edc34d080fcd3a8c469e4a1b78528c
Lusiker <1044114668@qq.com>
2023-01-04 01:12:33 +0800

    Update User.java
    User Entity finished. New Constructors may be needed in future.

--- src/main/java/com/campfire/campafar/Entity/User.java
+++ src/main/java/com/campfire/campafar/Entity/User.java
@@ -1,13 +1,43 @@
 package com.campfire.campafar.Entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.campfire.campafar.Enum.UserStateEnum;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor
+@TableName(schema = "campafar",value = "user")
 public class User {
+    @TableId(type = IdType.AUTO)
+    private Integer userId;
+    private String userName;
+    private String userEmail;
+    private String userPassword;
+    private String userIntroduction;
+    private Date createdAt;
+    private Date lastLogin;
+    private Integer totalLogin;
+    private Boolean userHasActivated;
+    private UserStateEnum userState;
+    private Integer userExp;
+    private BigDecimal userPoint;
+    private Boolean userQuestionOpen;
+    private BigDecimal userQuestionPrice;
+
+    public User(String userName, String userEmail, String userPassword) {
+        this.userName = userName;
+        this.userEmail = userEmail;
+        this.userPassword = userPassword;
+        this.userState = UserStateEnum.RESTRICTED;
+    }
 }
