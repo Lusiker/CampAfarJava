b394527496cddec4a963c8f49769edaa8feaae15
Lusiker <1044114668@qq.com>
2023-01-04 01:10:47 +0800

    Update CampAfarApplication.java
    Annotations added from mybatis-plus mapper.

--- src/main/java/com/campfire/campafar/CampAfarApplication.java
+++ src/main/java/com/campfire/campafar/CampAfarApplication.java
@@ -1,9 +1,13 @@
 package com.campfire.campafar;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
+@MapperScan("com.campfire.campafar.Mapper")
 public class CampAfarApplication {
 
     public static void main(String[] args) {
