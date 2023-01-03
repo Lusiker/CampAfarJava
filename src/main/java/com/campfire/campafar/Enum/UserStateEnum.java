98c2cd6df26ca6f598ae28a42c68400303bd93bd
Lusiker <1044114668@qq.com>
2023-01-04 01:12:37 +0800

    Update UserStateEnum.java

--- src/main/java/com/campfire/campafar/Enum/UserStateEnum.java
+++ src/main/java/com/campfire/campafar/Enum/UserStateEnum.java
@@ -1,4 +1,23 @@
 package com.campfire.campafar.Enum;
 
-public enum UserStateEnum {
+import com.baomidou.mybatisplus.annotation.EnumValue;
+import com.baomidou.mybatisplus.annotation.IEnum;
+
+public enum UserStateEnum implements IEnum<Byte> {
+    RESTRICTED(0),//用户尚未确认邮箱地址可用，受限
+    NORMAL(1),//用户状态正常
+    BANNED(2),//用户被封禁
+    LOGOFF(3);//用户已注销
+
+    @EnumValue
+    private final Byte value;
+
+    UserStateEnum(int value) {
+        this.value = (byte)value;
+    }
+
+    @Override
+    public Byte getValue() {
+        return this.value;
+    }
 }
