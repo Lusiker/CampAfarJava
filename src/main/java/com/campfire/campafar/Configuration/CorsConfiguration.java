37b910233b4d27780e6acd67256581c157a872f5
Lusiker <1044114668@qq.com>
2023-01-05 02:26:46 +0800

    Create CorsConfiguration.java

---
+++ src/main/java/com/campfire/campafar/Configuration/CorsConfiguration.java
@@ -0,0 +1,17 @@
+package com.campfire.campafar.Configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowCredentials(true)
+                .allowedMethods("GET","POST")
+                .maxAge(3600);
+    }
+}
