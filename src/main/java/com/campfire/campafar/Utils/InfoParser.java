62e2214adf4588f3f645f6c6a6f8e20290434c10
Lusiker <1044114668@qq.com>
2023-01-04 12:48:00 +0800

    Update InfoParser.java

--- src/main/java/com/campfire/campafar/Utils/InfoParser.java
+++ src/main/java/com/campfire/campafar/Utils/InfoParser.java
@@ -2,9 +2,14 @@ package com.campfire.campafar.Utils;
 
 /**
  * InfoParser类存放与parsing相关的方法
- * 在系统的主要位置，应当使用非基本数据类型以方便用判空方式处理错误
+ * 在系统的主要位置，应当使用封装的基本数据类型以方便用判空方式处理错误
  */
 public class InfoParser {
+    /**
+     * 将字符串转换为整数
+     * @param intStr： 输入的字符串
+     * @return result：转化后的整数
+     */
     public static Integer parseInt(String intStr){
         int result;
 
@@ -17,6 +22,11 @@ public class InfoParser {
         return result;
     }
 
+    /**
+     * 将字符串转换为short
+     * @param shortStr： 输入的字符串
+     * @return result：转化后的short型
+     */
     public static Short parseShort(String shortStr){
         short result;
 
@@ -29,6 +39,11 @@ public class InfoParser {
         return result;
     }
 
+    /**
+     * 将字符串转换为bool
+     * @param boolStr： 输入的字符串
+     * @return result：转化后的bool值
+     */
     public static Boolean parseBoolean(String boolStr){
         boolean result;
 
