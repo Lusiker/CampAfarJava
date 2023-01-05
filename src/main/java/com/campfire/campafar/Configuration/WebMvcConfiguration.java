66f08428cdb90022aba7f445683e75037088acea
Lusiker <1044114668@qq.com>
2023-01-05 16:18:57 +0800

    Update WebMvcConfiguration.java

---
+++ src/main/java/com/campfire/campafar/Configuration/WebMvcConfiguration.java
@@ -0,0 +1,33 @@
+package com.campfire.campafar.Configuration;
+
+import com.campfire.campafar.Utils.FileProcessor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+import java.io.File;
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //将获取 /files/avatar/** 的请求映射至 ../avatar/
+        File avatarPath = new File(FileProcessor.avatarStorePath);
+        
+        registry.addResourceHandler("/files/avatar/**")
+                .addResourceLocations("file:" + avatarPath.getAbsolutePath() + "\\")
+                .setCachePeriod(3600)
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver());
+    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowCredentials(true)
+                .allowedMethods("*")
+                .maxAge(3600);
+    }
+}
