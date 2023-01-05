c76f424bc586d160681796b8b9023b5e04284a46
Lusiker <1044114668@qq.com>
2023-01-05 22:45:26 +0800

    Update index.js

--- src/main/resources/front/index-master/src/store/index.js
+++ src/main/resources/front/index-master/src/store/index.js
@@ -3,15 +3,134 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
+const actions = {
+  login(context,newUser){
+    if(!state.loginState){
+      context.commit('LOGIN',newUser)
+    }
+  },
+  logout(context){
+    if(state.loginState){
+      context.commit('LOGOUT')
+    }
+  },
+  tryRetrieveUserInfo(context){
+    if(!state.loginState){
+      context.commit('TRY_RETRIEVE_USER_INFO')
+    }
+  },
+}
+
+const mutations = {
+  LOGIN(state,newUser){
+    state.loginState = true
+
+    state.currentUser.userId = newUser.userId
+    state.currentUser.userName = newUser.userName
+    state.currentUser.userEmail = newUser.userEmail
+    state.currentUser.userHasActivated = newUser.userHasActivated
+    state.currentUser.userState = newUser.userState
+    state.currentUser.userIntroduction = newUser.userIntroduction
+    state.currentUser.userExp = newUser.userExp
+    state.currentUser.userPoint = newUser.userPoint
+    state.currentUser.userQuestionOpen = newUser.userQuestionOpen
+    state.currentUser.userQuestionPrice = newUser.userQuestionPrice
+    state.currentUser.lastLogin = new Date(newUser.lastLogin)
+    state.currentUser.lastLoginString = newUser.lastLoginString
+    state.userArticleCount = newUser.userArticleCount
+    state.userFollowerCount = newUser.userFollowerCount
+    state.userFollowingCount = newUser.userFollowingCount
+
+    sessionStorage.setItem('loginState',true.toString())
+    for(let key in state.currentUser){
+      sessionStorage.setItem(key,state.currentUser[key].toString())
+    }
+  },
+
+  LOGOUT(state) {
+    state.loginState = false
+
+    state.currentUser = {
+      userId: 0,
+      userName: "",
+      userEmail: "",
+      userHasActivated: false,
+      userState: -1,
+      userIntroduction: "",
+      userArticleCount: 0,
+      userFollowerCount: 0,
+      userFollowingCount: 0,
+      userExp: 0,
+      userPoint: "0.00",
+      userQuestionOpen: false,
+      userQuestionPrice: "0.00",
+      lastLogin: new Date(),
+      lastLoginString: "",
+    }
+
+    sessionStorage.setItem('loginState',false.toString())
+    for(let key in state.currentUser){
+      sessionStorage.setItem(key,state.currentUser[key].toString())
+    }
+  },
+  TRY_RETRIEVE_USER_INFO(state){
+    if(sessionStorage.getItem('loginState') === "true"){
+      state.loginState = true
+
+      state.currentUser = {
+        userId: Number(sessionStorage.getItem('userId')),
+        userName: sessionStorage.getItem('userName'),
+        userEmail: sessionStorage.getItem('userEmail'),
+        userHasActivated: Boolean(sessionStorage.getItem('userHasActivated')),
+        userState: Number(sessionStorage.getItem('userState')),
+        userIntroduction: sessionStorage.getItem('userIntroduction'),
+        userFollowerCount: Number(sessionStorage.getItem('userFollowerCount')),
+        userFollowingCount: Number(sessionStorage.getItem('userFollowingCount')),
+        userArticleCount: Number(sessionStorage.getItem('userArticleCount')),
+        userExp: Number(sessionStorage.getItem('userExp')),
+        userPoint: Number(sessionStorage.getItem('userPoint')),
+        lastLogin: new Date(sessionStorage.getItem('lastLogin')),
+        lastLoginString: sessionStorage.getItem('lastLoginString'),
+        userQuestionPrice: sessionStorage.getItem('userQuestionPrice'),
+        userQuestionOpen: Boolean(sessionStorage.getItem('userQuestionOpen')),
+      }
+    }
+  },
+}
+
+const state = {
+    loginState: false,
+    currentUser: {
+      userId: 0,
+      userName: "",
+      userEmail: "",
+      userHasActivated: false,
+      userState: -1,
+      userIntroduction: "",
+      userArticleCount: 0,
+      userFollowerCount: 0,
+      userFollowingCount: 0,
+      userExp: 0,
+      userPoint: "0.00",
+      userQuestionOpen: false,
+      userQuestionPrice: "0.00",
+      lastLogin: new Date(),
+      lastLoginString: "",
+    }
+}
+
+const getters = {
+  loginState(state){
+    return state.loginState
+  },
+  user(state){
+    return state.currentUser
+  },
+}
+
 export default new Vuex.Store({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  },
+  actions,
+  mutations,
+  state,
+  getters
 });
