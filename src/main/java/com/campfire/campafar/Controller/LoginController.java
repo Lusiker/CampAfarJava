5d87b5c0fa89b8d0efe217682d8e605206e5accd
Lusiker <1044114668@qq.com>
2023-01-05 22:44:44 +0800

    Update LoginController.java
    +登录会更新用户登录时间与总次数
    +增加了邮箱登录
    +基于Builder构建返回用户信息

--- src/main/java/com/campfire/campafar/Controller/LoginController.java
+++ src/main/java/com/campfire/campafar/Controller/LoginController.java
@@ -1,10 +1,13 @@
 package com.campfire.campafar.Controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.campfire.campafar.DTO.UserInfoWrapper;
 import com.campfire.campafar.Entity.User;
+import com.campfire.campafar.Enum.CommonPageState;
 import com.campfire.campafar.Enum.UserStateEnum;
 import com.campfire.campafar.Repository.UserRepository;
 import com.campfire.campafar.Service.UserService;
+import com.campfire.campafar.Utils.RequestResult;
 import com.campfire.campafar.common.R;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
 
 
 @Slf4j
@@ -35,7 +39,7 @@ public class LoginController {
     /**
      * 用户名登录
      **/
-    public R<User> usernameLogin(HttpServletRequest request, @RequestBody User user) throws JsonProcessingException {
+    public R<UserInfoWrapper> usernameLogin(@RequestBody User user) {
         //1、将页面提交的密码password进行md5加密处理
         String password = user.getUserPassword();
         password = DigestUtils.md5DigestAsHex(password.getBytes());
@@ -59,6 +63,69 @@ public class LoginController {
         if(user.getUserState() == UserStateEnum.LOGOFF){
             return R.error("账号已注销");
         }
-        return R.success(user);
+
+        //登录时间，登录总数更新
+        Date now = new Date();
+        userRepository.updateLogin(user, now);
+
+        UserInfoWrapper wrapper = new UserInfoWrapper.UserInfoWrapperBuilder()
+                .setUser(user)
+                .setUserCommonInfo()
+                .setUserLoginInfo()
+                .setArticleCount()
+                .setFollowInfo()
+                .setUserQuestionInfo()
+                .build();
+
+        return R.success(wrapper);
+    }
+
+    @RequestMapping("/email")
+    public String userLoginByEmail(@RequestParam(value = "email", defaultValue = "")String userEmail,
+                                   @RequestParam(value = "password", defaultValue = "")String password) throws JsonProcessingException {
+        //任意参数为空
+        if(userEmail.isEmpty() || password.isEmpty()) {
+            return objectMapper.writeValueAsString(new RequestResult(CommonPageState.FAILED,1,null));
+        }
+
+        //邮箱输入不合法
+        if(userEmail.length() > 50) {
+            return objectMapper.writeValueAsString(new RequestResult(CommonPageState.FAILED,1,null));
+        }
+
+        //密码输入不合法
+        if(password.length() < 6 || password.length() > 20) {
+            return objectMapper.writeValueAsString(new RequestResult(CommonPageState.FAILED,1,null));
+        }
+
+        //查找目标用户
+        User user = userRepository.selectUserByEmail(userEmail);
+        //用户不存在或已注销
+        if(user == null || user.getUserState() == UserStateEnum.LOGOFF){
+            return objectMapper.writeValueAsString(new RequestResult(CommonPageState.FAILED,2,null));
+        }
+
+
+        String encodedPassword = DigestUtils.md5DigestAsHex(password.getBytes());
+        if(encodedPassword.equals(user.getUserPassword())) {
+            //回传用户信息
+            //登录时间，登录总数更新
+            Date now = new Date();
+            userRepository.updateLogin(user, now);
+
+            UserInfoWrapper wrapper = new UserInfoWrapper.UserInfoWrapperBuilder()
+                    .setUser(user)
+                    .setUserCommonInfo()
+                    .setUserLoginInfo()
+                    .setArticleCount()
+                    .setFollowInfo()
+                    .setUserQuestionInfo()
+                    .build();
+
+            return objectMapper.writeValueAsString(new RequestResult(CommonPageState.SUCCESSFUL,0,wrapper));
+        } else {
+            //密码错误
+            return objectMapper.writeValueAsString(new RequestResult(CommonPageState.FAILED,3,null));
+        }
     }
 }
