06969022ee80c2ad26b33b82e55d55341c809f85
Lusiker <1044114668@qq.com>
2023-01-05 16:21:40 +0800

    Update ArticleStateEnum.java

--- src/main/java/com/campfire/campafar/Enum/ArticleStateEnum.java
+++ src/main/java/com/campfire/campafar/Enum/ArticleStateEnum.java
@@ -1,4 +1,22 @@
 package com.campfire.campafar.Enum;
 
-public enum ArticleStateEnum {
+import com.baomidou.mybatisplus.annotation.EnumValue;
+import com.baomidou.mybatisplus.annotation.IEnum;
+
+public enum ArticleStateEnum implements IEnum<Byte> {
+    ReVIEWING(0),//尚未确认邮箱地址可用，受限
+    NORMAL(1),//文章状态正常
+    DELETED(2);//文章已删除
+
+    @EnumValue
+    private final Byte value;
+
+    ArticleStateEnum(int value) {
+        this.value = (byte)value;
+    }
+
+    @Override
+    public Byte getValue() {
+        return this.value;
+    }
 }
