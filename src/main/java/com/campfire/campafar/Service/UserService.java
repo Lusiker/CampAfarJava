fa5bddcd8812d3c0903e98b454bec84d3a107b03
horn24 <53045686+horn24@users.noreply.github.com>
2023-01-05 17:07:21 +0800

    Create UserService.java

---
+++ src/main/java/com/campfire/campafar/Service/UserService.java
@@ -0,0 +1,7 @@
+package com.campfire.campafar.Service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.campfire.campafar.Entity.User;
+
+public interface UserService extends IService<User> {
+}
