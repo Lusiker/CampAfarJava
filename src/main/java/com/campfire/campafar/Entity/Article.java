5db92a0fef0c3578b257fcf64737ddf8c186713e
Lusiker <1044114668@qq.com>
2023-01-06 19:27:10 +0800

    Update Article.java

--- src/main/java/com/campfire/campafar/Entity/Article.java
+++ src/main/java/com/campfire/campafar/Entity/Article.java
@@ -2,6 +2,7 @@ package com.campfire.campafar.Entity;
 
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import com.campfire.campafar.Enum.ArticleStateEnum;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,6 +16,7 @@ import java.util.Date;
 @Setter
 @ToString
 @NoArgsConstructor
+@TableName(schema = "campafar",value = "article")
 public class Article {
     Integer userId;
     @TableId(type = IdType.AUTO)
@@ -30,10 +32,11 @@ public class Article {
     Boolean articleVisible;
     Date articleLastModified;
 
-    Article(int uid, String newTitle, String newDetail, boolean isFree, BigDecimal newPrice) {
+    public Article(int uid, String newTitle, String newDetail, int tags, boolean isFree, BigDecimal newPrice) {
         this.userId = uid;
         this.articleTitle = newTitle;
         this.articleDetail = newDetail;
+        this.articleTag = tags;
         this.articleIsFree = isFree;
         this.articlePrice = newPrice;
     }
