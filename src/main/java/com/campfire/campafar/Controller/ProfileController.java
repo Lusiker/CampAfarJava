bdeae374b36b9e67f034145ac95d5845766a68f8
horn24 <53045686+horn24@users.noreply.github.com>
2023-01-06 10:09:22 +0800

    Update ProfileController.java

--- src/main/java/com/campfire/campafar/Controller/ProfileController.java
+++ src/main/java/com/campfire/campafar/Controller/ProfileController.java
@@ -1,8 +1,33 @@
 package com.campfire.campafar.Controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.campfire.campafar.Entity.User;
+import com.campfire.campafar.Service.UserService;
+import com.campfire.campafar.common.R;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
 @RestController
+@RequestMapping("/userinfo")
 public class ProfileController {
+    @Autowired
+    private UserService userService;
     //用户相关接口
+    /**
+     * 根据id查询用户信息
+     * @param userId
+     * @return
+     */
+    @GetMapping("check/{userId}")
+    public R<User> getById(@PathVariable Integer userId){
+        log.info("根据id查询用户信息...");
+        User user = userService.getById(userId);
+        if(user != null){
+            return R.success(user);
+        }
+        return R.error("没有查询到对应用户信息");
+    }
 }
