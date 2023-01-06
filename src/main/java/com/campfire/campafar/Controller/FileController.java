0f8412a94fde21c1139dd36d64e2dc1e4245cb15
Lusiker <1044114668@qq.com>
2023-01-06 19:27:12 +0800

    Update FileController.java

--- src/main/java/com/campfire/campafar/Controller/FileController.java
+++ src/main/java/com/campfire/campafar/Controller/FileController.java
@@ -1,7 +1,10 @@
 package com.campfire.campafar.Controller;
 
+import com.campfire.campafar.Entity.Article;
 import com.campfire.campafar.Entity.User;
+import com.campfire.campafar.Enum.ArticleStateEnum;
 import com.campfire.campafar.Enum.CommonPageState;
+import com.campfire.campafar.Repository.ArticleRepository;
 import com.campfire.campafar.Repository.UserRepository;
 import com.campfire.campafar.Utils.FileProcessor;
 import com.campfire.campafar.Utils.InfoParser;
@@ -24,6 +27,8 @@ public class FileController {
     ObjectMapper objectMapper;
     @Resource
     UserRepository userRepository;
+    @Resource
+    ArticleRepository articleRepository;
 
     /**
      * 上传头像接口
@@ -48,7 +53,7 @@ public class FileController {
 
         Integer uid = InfoParser.parseInt(uidStr);
         if(uid == null){
-            //用户名无效
+            //用户id无效
             return objectMapper.writeValueAsString(new RequestResult(CommonPageState.FAILED,1,null));
         }
 
@@ -76,7 +81,7 @@ public class FileController {
     public String acquireAvatar(@RequestParam(value = "uid", defaultValue = "")String uidStr) throws JsonProcessingException {
         Integer uid = InfoParser.parseInt(uidStr);
         if(uid == null){
-            //用户名无效
+            //用户id无效
             return objectMapper.writeValueAsString(new RequestResult(CommonPageState.FAILED,1,null));
         }
 
@@ -92,5 +97,104 @@ public class FileController {
         }
 
         return objectMapper.writeValueAsString(new RequestResult(CommonPageState.SUCCESSFUL, 0, result));
+    }
+
+    //上传article的banner
+    @RequestMapping("/upload/articleBanner")
+    public String uploadArticleBanner(@RequestParam(value = "uid", defaultValue = "")String uidStr,
+                                      @RequestParam(value = "aid", defaultValue = "")String aidStr,
+                                      @RequestBody MultipartFile file) throws IOException {
+        if(file == null) {
+            //文件参数为空
+            return objectMapper.writeValueAsString(new RequestResult(CommonPageState.FAILED,1,null));
+        }
+
+        double fileSize = file.getSize();
+        double sizeAsMB = fileSize / 1024 / 1024;
+        if(sizeAsMB > 2.0) {
+            //文件长度过大
+            return objectMapper.writeValueAsString(new RequestResult(CommonPageState.FAILED, 3, null));
+        }
+
+        Integer uid = InfoParser.parseInt(uidStr);
+        if(uid == null){
+            //用户id无效
+            return objectMapper.writeValueAsString(new RequestResult(CommonPageState.FAILED,1,null));
+        }
+
+        Integer aid = InfoParser.parseInt(aidStr);
+        if(aid == null){
+            //文章id无效
+            return objectMapper.writeValueAsString(new RequestResult(CommonPageState.FAILED,1,null));
+        }
+
+        if(file.isEmpty()) {
+            //文件为空
+            return objectMapper.writeValueAsString(new RequestResult(CommonPageState.FAILED,1,null));
+        }
+
+        User user = userRepository.selectUserById(uid);
+        if(user == null){
+            //目标用户不存在
+            return objectMapper.writeValueAsString(new RequestResult(CommonPageState.FAILED, 2, null));
+        }
+
+        Article article = articleRepository.selectArticleById(aid);
+        if(article == null || article.getArticleState() == ArticleStateEnum.DELETED) {
+            //目标文章不存在或已被删除
+            return objectMapper.writeValueAsString(new RequestResult(CommonPageState.FAILED, 3, null));
+        }
+
+        if(!article.getUserId().equals(uid)) {
+            //无权修改
+            return objectMapper.writeValueAsString(new RequestResult(CommonPageState.ACCESS_DENIED, 1, null));
+        }
+
+        if(!FileProcessor.saveBanner(aid, file)) {
+            //文件创建失败，内部错误
+            return objectMapper.writeValueAsString(new RequestResult(CommonPageState.INTERNAL_ERROR,1,null));
+        }
+
+        //文件创建成功
+        return objectMapper.writeValueAsString(new RequestResult(CommonPageState.SUCCESSFUL,0,null));
+    }
+
+    @RequestMapping("/acquire/articleBanner")
+    public String acquireArticleBanner(@RequestParam(value = "aid", defaultValue = "")String aidStr,
+                                       @RequestParam(value = "asUrl", defaultValue = "false")String asUrlStr) throws JsonProcessingException  {
+        Boolean asUrl = InfoParser.parseBoolean(asUrlStr);
+        if(asUrl == null) {
+            //参数错误
+            return objectMapper.writeValueAsString(new RequestResult(CommonPageState.FAILED,1,null));
+        }
+        System.out.println(asUrl);
+
+        Integer aid = InfoParser.parseInt(aidStr);
+        if(aid == null){
+            //文章id无效
+            return objectMapper.writeValueAsString(new RequestResult(CommonPageState.FAILED,1,null));
+        }
+
+        Article article = articleRepository.selectArticleById(aid);
+        if(article == null || article.getArticleState() == ArticleStateEnum.DELETED) {
+            //目标文章不存在或已被删除
+            return objectMapper.writeValueAsString(new RequestResult(CommonPageState.FAILED, 2, null));
+        }
+
+        if(asUrl) {
+            String url = FileProcessor.getArticleBannerAsUrl(aid);
+            if(url == null) {
+                return objectMapper.writeValueAsString(new RequestResult(CommonPageState.SUCCESSFUL,0,null));
+            } else {
+                return objectMapper.writeValueAsString(new RequestResult(CommonPageState.SUCCESSFUL,0,url));
+            }
+        } else {
+            String result = FileProcessor.getArticleBannerAsBase64(aid);
+            if(result == null) {
+                return objectMapper.writeValueAsString(new RequestResult(CommonPageState.SUCCESSFUL,0,null));
+            } else {
+                return objectMapper.writeValueAsString(new RequestResult(CommonPageState.SUCCESSFUL,0,result));
+            }
+        }
     }
 }
