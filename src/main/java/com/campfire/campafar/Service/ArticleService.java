71d621abd1449e434e6b3706fd3ca2bd7bdc9200
horn24 <53045686+horn24@users.noreply.github.com>
2023-01-07 13:32:51 +0800

    Create ArticleService.java

---
+++ src/main/java/com/campfire/campafar/Service/ArticleService.java
@@ -0,0 +1,7 @@
+package com.campfire.campafar.Service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.campfire.campafar.Entity.Article;
+
+public interface ArticleService extends IService<Article> {
+}
