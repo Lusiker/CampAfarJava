11d591df13e65b41d0dcef72027dc9e8e4cbd8d7
Lusiker <1044114668@qq.com>
2023-01-07 21:56:26 +0800

    Update UsersView.vue

--- src/main/resources/front/index-master/src/views/UsersView.vue
+++ src/main/resources/front/index-master/src/views/UsersView.vue
@@ -5,6 +5,7 @@
       <img src="../assets/img/icon-back.png" alt="" @click="toBack" />
       <div class="text">个人中心</div>
     </div>
+
     <div class="user-avatar">
       <div class="value">
         <img :src="avatarSrc" alt="" />
@@ -12,30 +13,42 @@
       </div>
       <div class="text">点击更换头像</div>
     </div>
+
     <div class="form-box">
       <div class="tit">账户信息</div>
-      <div class="group-item">
+      <div class="group-item" @click="showChangeNameDrawer = !showChangeNameDrawer">
         <div class="name">昵称</div>
         <div class="value">
           <span>{{ user.userName }}</span
           ><img src="../assets/img/icon-prev.png" class="arrow" />
         </div>
       </div>
-      <div class="group-item">
+
+      <div class="group-item" @click="showChangeEmailDrawer = !showChangeEmailDrawer">
         <div class="name">更换邮箱</div>
         <div class="value">
           <span>{{ user.userEmail }}</span
           ><img src="../assets/img/icon-prev.png" class="arrow" />
         </div>
       </div>
-      <div class="group-item">
+
+      <div class="group-item" @click="showChangeUserIntroductionDrawer = !showChangeUserIntroductionDrawer">
+        <div class="name">更换个人简介</div>
+        <div class="value">
+          <span>{{ user.userIntroduction }}</span
+          ><img src="../assets/img/icon-prev.png" class="arrow" />
+        </div>
+      </div>
+
+      <div class="group-item" @click="showActivateUserDrawer = !showActivateUserDrawer">
         <div class="name">激活邮箱</div>
         <div class="value">
           <span>当前状态:{{ user.userHasActivated ? '已激活' : '未激活' }}</span
           ><img src="../assets/img/icon-prev.png" class="arrow" />
         </div>
       </div>
-      <div class="group-item">
+
+      <div class="group-item" @click="showChangePasswordDrawer = !showChangePasswordDrawer">
         <div class="name">修改密码</div>
         <div class="value">
           <span class="un">设置密码</span><img src="../assets/img/icon-prev.png" class="arrow" />
@@ -46,23 +59,142 @@
         <van-button type="danger" block @click="logout">退出登录</van-button>
       </div>
     </div>
+
+
+    <van-action-sheet v-model="showChangeNameDrawer" title="更改用户名">
+      <van-col class="submit-button">
+        <van-row>
+          <van-field label="输入新用户名" :placeholder="user.userName" v-model="newUserName"></van-field>
+          <van-notice-bar
+              :text="'注意：用户名不应超过30（当前' + newUserName.length + ')'"
+              v-if="newUserName.length !== 0"
+          />
+        </van-row>
+        <br/><br/>
+        <van-divider/>
+        <van-row>
+        <div><van-button
+            :disabled="newUserName.length ===0 || newUserName.length > 30"
+            :color="'#6598e5'"
+            block
+            @click="changeUserName"
+        >提交</van-button></div>
+        </van-row>
+      </van-col>
+    </van-action-sheet>
+
+    <van-action-sheet v-model="showChangeEmailDrawer" title="更改邮箱">
+      <van-col class="submit-button">
+        <van-row>
+          <van-field label="输入新邮箱" :placeholder="user.userEmail" v-model="newUserEmail"></van-field>
+          <van-notice-bar
+              :text="'注意：邮箱不应超过50（当前' + newUserEmail.length + ')'"
+              v-if="newUserName.length !== 0"
+          />
+        </van-row>
+        <br/><br/>
+        <van-divider/>
+        <van-row>
+          <div><van-button
+              :disabled="newUserEmail.length ===0 || newUserEmail.length > 50"
+              :color="'#6598e5'"
+              block
+              @click="changeUserEmail"
+          >提交</van-button></div>
+        </van-row>
+      </van-col>
+    </van-action-sheet>
+
+    <van-action-sheet v-model="showChangeUserIntroductionDrawer" title="更改个人简介">
+      <van-col class="submit-button">
+        <van-row>
+          <van-field label="输入个人简介" :placeholder="user.userIntroduction" v-model="newUserIntroduction"></van-field>
+          <van-notice-bar
+              :text="'注意：个人简介不应超过250（当前' + newUserIntroduction.length + ')'"
+              v-if="newUserIntroduction.length !== 0"
+          />
+        </van-row>
+        <br/><br/>
+        <van-divider/>
+        <van-row>
+          <div><van-button
+              :disabled="newUserIntroduction.length ===0 || newUserIntroduction.length > 250"
+              :color="'#6598e5'"
+              block
+              @click="changeUserIntroduction"
+          >提交</van-button></div>
+        </van-row>
+      </van-col>
+    </van-action-sheet>
+
+    <van-action-sheet v-model="showActivateUserDrawer" title="激活邮箱">
+      <van-col class="submit-button">
+        <van-row>
+          <van-field label="输入激活码" v-model="activateCode">
+            <template #button>
+              <van-button>获取激活码</van-button>
+            </template>
+          </van-field>
+        </van-row>
+        <van-divider/>
+        <van-row>
+          <div><van-button
+              :disabled="activateCode.length ===0 || activateCode.length > 4"
+              :color="'#6598e5'"
+              block
+              @click="sendCode"
+          >提交</van-button></div>
+        </van-row>
+      </van-col>
+    </van-action-sheet>
+
+    <van-action-sheet v-model="showChangePasswordDrawer" title="更换密码">
+      <van-col class="submit-button">
+        <van-row>
+          <van-field label="输入新密码" type="password" v-model="newPassword"></van-field>
+          <van-field label="重复新密码" type="password" v-model="newPasswordRepeat"></van-field>
+          <van-notice-bar
+              :text="'注意：密码长度2介于6到20之间（当前' + newPassword.length + ')'"
+              v-if="newPassword.length !== 0"
+          />
+        </van-row>
+        <br/><br/>
+        <van-divider/>
+        <van-row>
+          <div><van-button
+              :disabled="newPassword.length === 0 || newPassword !== newPasswordRepeat || newPassword.length > 20 || newPassword.length < 6"
+              :color="'#6598e5'"
+              block
+              @click="changeUserEmail"
+          >提交</van-button></div>
+        </van-row>
+      </van-col>
+    </van-action-sheet>
   </div>
+
 </template>
 
 <script>
-//这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
-//例如：import 《组件名称》 from '《组件路径》';
+import { Toast } from 'vant';
 
 export default {
-  //import引入的组件需要注入到对象中才能使用
-  components: {},
   data() {
-    //这里存放数据
     return {
       file: "",
+      showChangeNameDrawer: false,
+      newUserName: '',
+      showChangeEmailDrawer: false,
+      newUserEmail: '',
+      showChangeUserIntroductionDrawer: false,
+      newUserIntroduction: '',
+      showActivateUserDrawer: false,
+      showChangePasswordDrawer: false,
+      newPassword: '',
+      newPasswordRepeat: '',
+      activateCode: '',
+
     };
   },
-  //监听属性 类似于data概念
   computed: {
     loginState() {
       return this.$store.getters.loginState
@@ -74,10 +206,11 @@ export default {
       return this.$store.getters.avatar
     }
   },
-  //监控data中的数据变化
   watch: {},
-  //方法集合
   methods: {
+    toastUpdateFail() {
+      Toast.fail("更新失败")
+    },
     logout(){
       this.$store.dispatch('logout')
       this.$router.push({
@@ -100,6 +233,69 @@ export default {
             });
       }
     },
+    changeUserName() {
+      if(this.loginState) {
+        this.$request.post('/user/update?uid=' + this.user.userId + "&userName=" + this.newUserName).then(
+            (res) => {
+              switch (res.stateEnum.state) {
+                case 0: {
+                  this.$store.dispatch('changeUserName', this.newUserName)
+                  this.newUserName = ''
+                  this.showChangeNameDrawer = false
+                  break
+                }
+                default: {
+                  this.toastUpdateFail()
+                }
+              }
+            }
+        )
+      }
+    },
+    changeUserEmail() {
+      if(this.loginState) {
+        this.$request.post('/user/update?uid=' + this.user.userId + "&userEmail=" + this.newUserEmail).then(
+            (res) => {
+              switch (res.stateEnum.state) {
+                case 0: {
+                  this.$store.dispatch('changeUserEmail', this.newUserEmail)
+                  this.newUserEmail = ''
+                  this.showChangeEmailDrawer = false
+                  break
+                }
+                default: {
+                  this.toastUpdateFail()
+                }
+              }
+            }
+        )
+      }
+    },
+    changeUserIntroduction() {
+      if(this.loginState) {
+        this.$request.post('/user/update?uid=' + this.user.userId + "&userIntroduction=" + this.newUserIntroduction).then(
+            (res) => {
+              switch (res.stateEnum.state) {
+                case 0: {
+                  this.$store.dispatch('changeUserIntroduction', this.newUserIntroduction)
+                  this.newUserIntroduction = ''
+                  this.showChangeUserIntroductionDrawer = false
+                  break
+                }
+                default: {
+                  this.toastUpdateFail()
+                }
+              }
+            }
+        )
+      }
+    },
+    requestActivateCode() {
+
+    },
+    verifyActivateCode() {
+
+    }
   },
   //生命周期 - 挂载完成（可以访问DOM元素）
   mounted() {
@@ -138,6 +334,10 @@ export default {
 };
 </script>
 <style lang="less" scoped>
+ .submit-button {
+   width: 100%;
+   text-align: center;
+ }
 div.container {
   box-sizing: border-box;
   padding-top: 50px;
