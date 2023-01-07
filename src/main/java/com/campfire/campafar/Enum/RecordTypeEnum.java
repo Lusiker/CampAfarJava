02a39e71d204bb4e4fe328f667c2dd832350bba9
Lusiker <1044114668@qq.com>
2023-01-08 00:48:03 +0800

    Update RecordTypeEnum.java

--- src/main/java/com/campfire/campafar/Enum/RecordTypeEnum.java
+++ src/main/java/com/campfire/campafar/Enum/RecordTypeEnum.java
@@ -5,7 +5,7 @@ import com.baomidou.mybatisplus.annotation.IEnum;
 
 
 public enum RecordTypeEnum implements IEnum<Byte> {
-    REGISTRY(0),//注册用验证码
+    ACTIVATION(0),//激活验证码
     FORGET_PASSWORD(1),//忘记密码用验证码
     CHARGE(2);//充值用验证码
 
