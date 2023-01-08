44f3c6696ae1dc1c8429c7d2bfb7d46714101e19
Lusiker <1044114668@qq.com>
2023-01-08 16:22:17 +0800

    Update create_table_purchase.sql

--- src/main/resources/sql/create_table_purchase.sql
+++ src/main/resources/sql/create_table_purchase.sql
@@ -6,5 +6,7 @@ create table purchase (
     target_type tinyint(2) NOT NULL, #支付类别，文章、提问、旁听
     target_id int unsigned NOT NULL, #对象id
     purchase_created_at timestamp NOT NULL, #支付创建时间
+    purchase_state tinyint(2) NOT NULL DEFAULT 0, #支付状态
+    purchase_finished_at timestamp default null, #支付完成于
     purchase_serial_id varchar(50) NOT NULL #总感觉支付应该有个生成的字符串很有逼格，不知道怎么搞就先留空
 );
