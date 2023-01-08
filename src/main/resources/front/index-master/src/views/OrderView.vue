86f1b37106e8c6186dfb43716440ca9756ca1fd2
Lusiker <1044114668@qq.com>
2023-01-08 23:55:57 +0800

    Update OrderView.vue

--- src/main/resources/front/index-master/src/views/OrderView.vue
+++ src/main/resources/front/index-master/src/views/OrderView.vue
@@ -51,25 +51,6 @@ export default {
       });
     },
   },
-  //生命周期 - 创建完成（可以访问当前this实例）
-  async created() {
-    let res = await this.$request.get("http://1.14.239.98/api/v2/member/orders?", {
-      params: {
-        page: 1,
-        page_size: 500,
-      },
-    });
-    this.mains = res.data.data;
-  },
-  //生命周期 - 挂载完成（可以访问DOM元素）
-  mounted() {},
-  beforeCreate() {}, //生命周期 - 创建之前
-  beforeMount() {}, //生命周期 - 挂载之前
-  beforeUpdate() {}, //生命周期 - 更新之前
-  updated() {}, //生命周期 - 更新之后
-  beforeDestroy() {}, //生命周期 - 销毁之前
-  destroyed() {}, //生命周期 - 销毁完成
-  activated() {}, //如果页面有keep-alive缓存功能，这个函数会触发
 };
 </script>
 <style lang="less" scoped>
