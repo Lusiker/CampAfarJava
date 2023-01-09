d06fdb8415fe139464308b8e359cad9cdbb8de93
Lusiker <1044114668@qq.com>
2023-01-09 13:33:04 +0800

    Update OtherUserView.vue

--- src/main/resources/front/index-master/src/views/OtherUserView.vue
+++ src/main/resources/front/index-master/src/views/OtherUserView.vue
@@ -32,6 +32,12 @@
             <span>{{user.userIntroduction}}</span>
           </div>
         </div>
+        <div class="group-item">
+          <div class="name">上次登录</div>
+          <div class="value">
+            <span>{{user.lastLoginString}}</span>
+          </div>
+        </div>
         <div class="group-item" @click="showArticleList = !showArticleList">
           <div class="name">Ta的文章</div>
           <div class="value">
