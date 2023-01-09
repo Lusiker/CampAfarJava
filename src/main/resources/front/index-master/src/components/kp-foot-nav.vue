22a5fd8fd9de4073f99dff0d6ec12cbe733488ca
Lusiker <1044114668@qq.com>
2023-01-09 11:20:04 +0800

    Update kp-foot-nav.vue

--- src/main/resources/front/index-master/src/components/kp-foot-nav.vue
+++ src/main/resources/front/index-master/src/components/kp-foot-nav.vue
@@ -20,20 +20,13 @@
 </template>
 
 <script>
-//这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
-//例如：import 《组件名称》 from '《组件路径》';
-
 export default {
   props: ["type"],
-  //import引入的组件需要注入到对象中才能使用
   components: {},
   data() {
-    //这里存放数据
     return {};
   },
-  //监听属性 类似于data概念
   computed: {},
-  //监控data中的数据变化
   watch: {},
   //方法集合
   methods: {
@@ -42,18 +35,7 @@ export default {
         name,
       });
     },
-  },
-  //生命周期 - 创建完成（可以访问当前this实例）
-  created() {},
-  //生命周期 - 挂载完成（可以访问DOM元素）
-  mounted() {},
-  beforeCreate() {}, //生命周期 - 创建之前
-  beforeMount() {}, //生命周期 - 挂载之前
-  beforeUpdate() {}, //生命周期 - 更新之前
-  updated() {}, //生命周期 - 更新之后
-  beforeDestroy() {}, //生命周期 - 销毁之前
-  destroyed() {}, //生命周期 - 销毁完成
-  activated() {}, //如果页面有keep-alive缓存功能，这个函数会触发
+  }
 };
 </script>
 <style lang="less" scoped>
@@ -63,11 +45,13 @@ export default {
   left: 0;
   right: 0;
   width: 100%;
-  height: 53px;
+  height: auto;
   background: #ffffff;
   display: flex;
   flex-direction: row;
   justify-content: space-around;
+  z-index: 0;
+  margin-top: 60px;
   .item {
     height: 53px;
     display: flex;
